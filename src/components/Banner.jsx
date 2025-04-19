8ae11bc16437e926f3f415053d8747be47be254d
Gourav Kumar <raj11gourav44@gmail.com>
2025-04-19 12:52:32 +0530

    banner image error resolved

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ export function Banner() {
       className="h-[20vh] md:h-[80vh] bg-cover flex items-end"
       style={{
         backgroundImage:
-          "url(https://images4.alphacoders.com/130/1300727.jpg)",
+          "url(https://thumbnails.cbsig.net/CBS_Production_Entertainment_VMS/2022/10/26/2091444291941/TGMAV_SAlone_16_9_1920x1080_1781067_1920x1080.jpg)",
       }}
     >
         <div className="text-white text-xl text-center w-full bg-gray-900/60 p-3">
